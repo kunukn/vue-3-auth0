8cdb028f847933fbbb7a843d3971863ce9add397
Kunuk Nykjær <knyk@novonordisk.com>
2024-04-16 13:48:31 +0200

    update

--- create-env-file.js
+++ create-env-file.js
@@ -5,16 +5,9 @@ const filename = ".env";
 const content = `
 AUTH0_DOMAIN=1
 AUTH0_CLIENT_ID=2
-MY_CALLBACK_URL=3
+CALLBACK_URL=3
 `;
 
-fs.unlink(filename, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('File deleted successfully');
-  });
 
 fs.access(filename, fs.F_OK, (err) => {
   if (err) {
@@ -23,7 +16,7 @@ fs.access(filename, fs.F_OK, (err) => {
         console.error(err);
         return;
       }
-      console.log(".env file was created");
+      console.log(".env file was created", content);
     });
   }
 });
--- src/main.js
+++ src/main.js
@@ -5,18 +5,18 @@ import { createApp } from "vue";
 import { createAuth0 } from "@auth0/auth0-vue";
 import router from "./router";
 
-console.log(import.meta.env.VITE_SOME_KEY) 
+console.log(import.meta.env.VITE_SOME_KEY);
 
-const app = createApp(App)
-  .use(
-    createAuth0({
-      domain: "<AUTH0_DOMAIN>",
-      clientId: "<AUTH0_CLIENT_ID>",
-      authorizationParams: {
-        redirect_uri: "<MY_CALLBACK_URL>",
-      },
-    })
-  )
-  .use(router);
+const auth0Config = {
+  domain: import.meta.env.AUTH0_DOMAIN,
+  clientId: import.meta.env.AUTH0_CLIENT_ID,
+  authorizationParams: {
+    redirect_uri: import.meta.env.CALLBACK_URL,
+  },
+};
+
+console.log(auth0Config);
+
+const app = createApp(App).use(createAuth0(auth0Config)).use(router);
 
 app.mount("#app");
