372c014b107b1b9d164cfb239331a9f5605d225f
Kunuk Nykjær <kunukn@gmail.com>
2024-04-24 10:42:06 +0200

    cleanup

--- src/router.js
+++ src/router.js
@@ -6,25 +6,18 @@ import HomePage from '@/pages/HomePage.vue'
 import LoginPage from '@/pages/LoginPage.vue'
 import { createAuthGuard } from '@auth0/auth0-vue'
 
-const routes = [
-  { name: 'home', path: '/', component: HomePage },
-  { name: 'login', path: '/login', component: LoginPage },
-  {
-    name: 'api',
-    path: '/api',
-    component: ApiPage,
-    beforeEnter: createAuthGuard(app),
-  },
-  { name: 'callback', path: '/auth-callback', component: AuthCallbackPage },
-]
-
 export function createRouter(app) {
   return createVueRouter({
     routes: [
-      { path: '/', component: HomePage },
-      { path: '/login', component: LoginPage },
-      { path: '/api', component: ApiPage, beforeEnter: createAuthGuard(app) },
-      { path: '/auth-callback', component: AuthCallbackPage },
+      { name: 'home', path: '/', component: HomePage },
+      { name: 'login', path: '/login', component: LoginPage },
+      {
+        name: 'api',
+        path: '/api',
+        component: ApiPage,
+        beforeEnter: createAuthGuard(app),
+      },
+      { name: 'callback', path: '/auth-callback', component: AuthCallbackPage },
     ],
     history: createWebHistory(),
   })
